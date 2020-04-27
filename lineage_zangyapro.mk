a060862628b1224b3486b6dc59a2170968e497bc
Danct12 <danct12@disroot.org>
2020-04-27 17:07:42 +0700

    ubport for Zangyapro

--- lineage_zangyapro.mk
+++ lineage_zangyapro.mk
@@ -1,9 +1,9 @@
 # Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+#$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Vendor blobs
 $(call inherit-product-if-exists, vendor/bq/zangyapro/zangyapro-vendor.mk)
